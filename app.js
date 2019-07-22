c5c93ba4a49a4da4dcab05bb36ba808f0ff76b1f
Gabriel <gabrielbp+github@gmail.com>
2019-07-22 17:49:32 -0300

    Section 2 - 10 - Numbers and Booleans

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-//string
+// string
 var myName = 'Gabriel';
-myName = 29;
+//myName = 29;
+// number
+var myAge = 27; // can also be floating point number - TypeScript doesn't differ between them 
+//myAge = 'Gabriel';
+// boolean 
+var hasHobbies = false;
+hasHobbies = 1; // gives error even though you can cast it
--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
-//string
+// string
 let myName = 'Gabriel';
-myName = 29;
+//myName = 29;
 
+// number
+let myAge = 27; // can also be floating point number - TypeScript doesn't differ between them 
+//myAge = 'Gabriel';
+
+// boolean 
+let hasHobbies = false;
+hasHobbies = 1; // gives error even though you can cast it

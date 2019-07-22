f287535164297de3b51f37d8dac5e91cb1383459
Gabriel <gabrielbp+github@gmail.com>
2019-07-22 18:07:13 -0300

    Section 2 - 11 - Assigning Types Explicitly

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ var myAge = 27; // can also be floating point number - TypeScript doesn't differ
 //myAge = 'Gabriel';
 // boolean 
 var hasHobbies = false;
-hasHobbies = 1; // gives error even though you can cast it
+//hasHobbies = 1; // gives error even though you can cast it
+// assign types
+//let myRealAge; //type any
+var myRealAge; //explicit assinging type to variable
+myRealAge = 27;
+//myRealAge = '27'; //no error from tsc
--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
 // string
-let myName = 'Gabriel';
+let myName: string = 'Gabriel';
 //myName = 29;
 
 // number
-let myAge = 27; // can also be floating point number - TypeScript doesn't differ between them 
+let myAge: number = 27; // can also be floating point number - TypeScript doesn't differ between them 
 //myAge = 'Gabriel';
 
 // boolean 
-let hasHobbies = false;
-hasHobbies = 1; // gives error even though you can cast it
+let hasHobbies: boolean = false;
+//hasHobbies = 1; // gives error even though you can cast it
+
+// assign types
+//let myRealAge; //type any
+let myRealAge: number; //explicit assinging type to variable
+myRealAge = 27;
+//myRealAge = '27'; //no error from tsc

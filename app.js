d81ebda2ab229fa0b8bac2fa37975d0f137ade67
Gabriel <gabrielbp+github@gmail.com>
2019-07-22 18:34:35 -0300

    Section 2 - 12 - Arrays and Types

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ var myAge = 27; // can also be floating point number - TypeScript doesn't differ
 var hasHobbies = false;
 //hasHobbies = 1; // gives error even though you can cast it
 // assign types
-//let myRealAge; //type any
-var myRealAge; //explicit assinging type to variable
+//let myRealAge; // type any
+var myRealAge; // explicit assinging type to variable
 myRealAge = 27;
-//myRealAge = '27'; //no error from tsc
+//myRealAge = '27'; // no error from tsc
+// array
+//let hobbies = ["Cooking", "Sports"]; // TypeScript infers this to be an array of strings
+//can override this behavior with
+var hobbies = ["Cooking", "Sports"]; // it is not an array of strings only anymore
+//console.log(hobbies[0]);
+//console.log(typeof hobbies);
+hobbies = [100]; // TypeScript doesn't allow other types in this array
+//hobbies = 100; // still gives error
--- app.ts
+++ app.ts
@@ -11,7 +11,19 @@ let hasHobbies: boolean = false;
 //hasHobbies = 1; // gives error even though you can cast it
 
 // assign types
-//let myRealAge; //type any
-let myRealAge: number; //explicit assinging type to variable
+//let myRealAge; // type any
+let myRealAge: number; // explicit assinging type to variable
 myRealAge = 27;
-//myRealAge = '27'; //no error from tsc
+//myRealAge = '27'; // no error from tsc
+
+
+// array
+//let hobbies = ["Cooking", "Sports"]; // TypeScript infers this to be an array of strings
+
+//can override this behavior with
+let hobbies: any[] = ["Cooking", "Sports"]; // it is not an array of strings only anymore
+
+//console.log(hobbies[0]);
+//console.log(typeof hobbies);
+hobbies = [100]; // TypeScript doesn't allow other types in this array
+//hobbies = 100; // still gives error

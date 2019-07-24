2c067a1dda510761a5d8c223a285afb54aab490d
Gabriel <gabrielbp+github@gmail.com>
2019-07-24 16:23:48 -0300

    Section 3 - 41 - 'Let' and 'Const'

---
+++ app.js
@@ -0,0 +1,11 @@
+// Section 4 - 41 - "Let" and "Const"
+// TypeScript let's you use ES6 while generation other ES versions
+// let creates a block variable
+// var creates a global variable
+var variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+var maxLevels = 100;
+console.log(maxLevels);
+maxLevels = 99;
---
+++ app.ts
@@ -0,0 +1,12 @@
+// Section 4 - 41 - "Let" and "Const"
+// TypeScript let's you use ES6 while generation other ES versions
+// let creates a block variable
+// var creates a global variable
+let variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+
+const maxLevels = 100;
+console.log(maxLevels);
+//maxLevels = 99;

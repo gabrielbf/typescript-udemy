f0cc4603021216c967c15a9497ec3ac7899307ea
Gabriel <gabrielbp+github@gmail.com>
2019-07-24 16:30:11 -0300

    Section 3 - 42 - Block Scope

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 // TypeScript let's you use ES6 while generation other ES versions
 // let creates a block variable
 // var creates a global variable
+console.log("LET & CONST");
 var variable = "Test";
 console.log(variable);
 variable = "Another value";
 console.log(variable);
 var maxLevels = 100;
 console.log(maxLevels);
-maxLevels = 99;
+//maxLevels = 99; // doesn't work
+// 42 Block Scope
+function reset() {
+    //console.log(variable); // undefined because was declared before the function block
+    var variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable); // returns "Another value"
--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@
 // TypeScript let's you use ES6 while generation other ES versions
 // let creates a block variable
 // var creates a global variable
+console.log("LET & CONST");
 let variable = "Test";
 console.log(variable);
 variable = "Another value";
@@ -9,4 +10,15 @@ console.log(variable);
 
 const maxLevels = 100;
 console.log(maxLevels);
-//maxLevels = 99;
+//maxLevels = 99; // doesn't work
+
+// 42 Block Scope
+function reset() {
+	//console.log(variable); // undefined because was declared before the function block
+	let variable = null;
+	console.log(variable);
+}
+reset();
+console.log(variable); // returns "Another value"
+
+// 43 Arrow Functions

d4ddbf40f0bcdcd6ab4164311a599aaa0848507c
Gabriel <gabrielbp+github@gmail.com>
2019-07-24 15:47:04 -0300

    Section 3 - 33 - Debugging your TypeScript Code using Source Maps

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var myName = "Max";
 var myAge = 27;
-myName = 30; // gives an error but compiles anyway - standard behavior
+// 32 Changing the Compiler Behavior on Errors
+//myName = 30; // gives an error but compiles anyway - standard behavior
+// tsconfig.json - add noEmitOnError True so tsc doesn't create file if it finds any error
+// 33 Debugging your TypeScript Code using Source Maps
+// tsconfig.json - sourceMap true
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
 var myName = "Max";
 var myAge = 27
 
-myName = 30; // gives an error but compiles anyway - standard behavior
+// 32 Changing the Compiler Behavior on Errors
+//myName = 30; // gives an error but compiles anyway - standard behavior
+// tsconfig.json - add noEmitOnError True so tsc doesn't create file if it finds any error
+
+// 33 Debugging your TypeScript Code using Source Maps
+// tsconfig.json - sourceMap true we get access to the ts file and can debug in the browser

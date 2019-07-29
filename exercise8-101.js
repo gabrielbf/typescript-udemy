100d2ec7a709a7688bda34d13bbcdae1ebc6ec0f
Gabriel <gabrielbp+github@gmail.com>
2019-07-29 19:31:58 -0300

    Section 6 - 102 - Module Exercise: Solution

---
+++ exercise8-101.js
@@ -0,0 +1,32 @@
+var MyMap = (function () {
+    function MyMap() {
+        this.map = {};
+    }
+    MyMap.prototype.setItem = function (key, item) {
+        this.map[key] = item;
+    };
+    MyMap.prototype.getItem = function (key) {
+        return this.map[key];
+    };
+    MyMap.prototype.clear = function () {
+        this.map = {};
+    };
+    MyMap.prototype.printMap = function () {
+        for (var key in this.map) {
+            console.log(key, this.map[key]);
+        }
+    };
+    return MyMap;
+}());
+var numberMap = new MyMap();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+numberMap.clear();
+numberMap.printMap();
+var stringMap = new MyMap();
+stringMap.setItem('apples', "10");
+stringMap.setItem('bananas', "2");
+stringMap.printMap();
+stringMap.clear();
+stringMap.printMap();

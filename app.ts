968a70108fd891f903f27885e848acd544f5df27
Gabriel <gabrielbp+github@gmail.com>
2019-07-30 18:53:01 -0300

    Section 11 - 133 - Adding TypeScript into a Gulp Workflow

--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
 import "jQuery";
 $("#app").css({"background-color": "green"});
+
+// 132 More on "tsc" and the tsconfig File
+// when using tsc app.ts, the tsconfig file is not used
+// use -p <location> to use tsconfig file in other directory
+
+// 133 Adding TypeScript into a Gulp Workflow

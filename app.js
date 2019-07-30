1b77e2ab2577f6e12e3a3803d5a0e9f9727f8f40
Gabriel <gabrielbp+github@gmail.com>
2019-07-30 15:58:10 -0300

    Section 10 - 116 - Declaring Variables

---
+++ app.js
@@ -0,0 +1,6 @@
+$('button').click(function () {
+    alert('Button was clicked');
+});
+$('button').click(function () {
+    alert('Button was clicked');
+});
--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 // Section 10 Using JavaScript Libraries (like jQuery) with TypeScript
 
 // 115 Using jQuery
+//$('button').click(function() {
+//	alert('Button was clicked')
+//})
+
+// doesn't work because TypeScript doesn't know about jQuery
+
+// 116 Declaring Variables
+declare var $: any; // easy and cheap way to solve it
+
 $('button').click(function() {
 	alert('Button was clicked')
 })
-
-// doesn't work because TypeScript doesn't know about jQuery
